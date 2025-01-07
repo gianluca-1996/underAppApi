2eefa3dc0a8de0906a293a9e0865dd7e1cfdab64
gianluca <gianluca@tfs.com>
2025-01-07 11:36:45 -0300

    eliminaPathPantalla

--- src/models/pantalla.model.js
+++ src/models/pantalla.model.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const coleccionPantalla = 'Pantalla';
 const pantallatSchema = new mongoose.Schema({
     nombre: {type: String, require: true},
-    path: {type: String, require: true},
     descripcion: {type: String, require: false},
     roles_permitidos: { type: [String], default: [] }
 });
